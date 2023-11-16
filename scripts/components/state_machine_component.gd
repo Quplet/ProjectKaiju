5b37a5d15dbe1b5a6ec8bd472e30f0e85f7f1371
Quplet <jonathanhart1114@gmail.com>
2023-11-16 12:01:04 -0500

    start of enemy ai, finite state machine

---
+++ scripts/components/state_machine_component.gd
@@ -0,0 +1,42 @@
+extends Node
+
+@export var initial_state: State
+
+var states: Dictionary = {}
+var current_state: State
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	for child in get_children():
+		if child is State:
+			states[child.name.to_lower()] = child
+			child.transitioned.connect(on_child_transition)
+	
+	if initial_state:
+		initial_state.enter()
+		current_state = initial_state
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float):
+	if current_state:
+		current_state.update(delta)
+		
+func _physics_process(delta: float):
+	if current_state:
+		current_state.physics_update(delta)
+		
+
+func on_child_transition(state: State, new_state_name: String):
+	if state != current_state:
+		return
+		
+	var new_state: State = states[new_state_name.to_lower()]
+	if !new_state:
+		return
+		
+	if current_state:
+		current_state.exit()
+		
+	new_state.enter()
+	current_state = new_state
--- scripts/objects/enemy.gd
+++ scripts/objects/enemy.gd
@@ -1,12 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+@export var speed_x: float = 200.0
+@export var speed_y: float = 50.0
 
 func _physics_process(delta):
 	velocity.x = move_toward(velocity.x, 0, 30)
